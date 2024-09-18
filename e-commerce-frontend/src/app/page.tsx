e6021f482c14f80fce89fb01d0c72e6b3dd4c26d
24LP2121 24LP2121 <24LP2121@administrators-iMac-3.local>
2024-09-18 17:40:29 +0800

    input

--- e-commerce-frontend/src/app/page.tsx
+++ e-commerce-frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Header } from "./components/header";
-import { Listitem } from "./components/listitem";
-import { Tableheader } from "./components/tableheader";
+import { Header } from "@/components/header";
+import { Listitem } from "@/components/listitem";
+
 export default function Home() {
   return (
     <main>
