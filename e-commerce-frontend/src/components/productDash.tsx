086c524510b08c4e6f9d578cb910a8e9c4e15523
Uuganaa-abo <aku.pbnn@gmail.com>
2024-09-20 12:08:12 +0800

    productDash update input, button

--- e-commerce-frontend/src/components/productDash.tsx
+++ e-commerce-frontend/src/components/productDash.tsx
@@ -9,18 +9,24 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import {
+  ArrowDown,
   BookMarked,
+  Calendar,
+  ChevronDown,
   DollarSign,
   LayoutGrid,
   LayoutList,
   Pencil,
   Plus,
+  Search,
   Settings,
+  Shapes,
   Trash2,
 } from "lucide-react";
 import { useState } from "react";
 import { ProductUpload } from "./productUpload";
 import { Checkbox } from "./ui/checkbox";
+import { Input } from "./ui/input";
 const invoices = [
   {
     invoice: "INV001",
@@ -70,7 +76,7 @@ export function Product() {
   const [open, SetOpen] = useState(false);
   return (
     <div>
-      <div className="flex w-full">
+      <div className="flex w-full bg-slate-400">
         <div className="max-w-56 flex flex-col gap-4 py-4 bg-slate-100">
           <div className="flex gap-2">
             <LayoutGrid className="w-6" />
@@ -95,7 +101,7 @@ export function Product() {
             <div className="text-base font-semibold">Тохиргоо</div>
           </div>
         </div>
-        <div className="w-full">
+        <div className="w-ful">
           <div className="flex gap-4 py-4">
             <p className="text-sm font-medium p-4 hover:font-semibold hover:border-b-2 border-black hover:cursor-pointer">
               Бүтээгдэхүүн
@@ -104,12 +110,35 @@ export function Product() {
               Ангилал
             </p>
           </div>
-          <div className="flex flex-col gap-6 ml-6">
+          <div className="flex flex-col gap-6 px-6 ml-6">
             <Button className="w-[280px] rounded-xl text-base font-semibold px-4 py-3">
               <div className="text-base font-semibold flex gap-1 px-7 py-2">
                 <Plus /> Бүтээгдэхүүн нэмэх
               </div>
             </Button>
+            <div className="flex justify-between items-center">
+              <div className="flex gap-3">
+                <div className="flex gap-2 py-2 px-3">
+                  <Shapes className="size-6 text-iconPrimary"/>
+                  <p>Ангилал</p>
+                  <ChevronDown className="size-6 text-iconPrimary"/>
+                </div>
+                <div  className="flex gap-2 py-2 px-3">
+                  <DollarSign className="size-6 text-iconPrimary"/>
+                  <p>Үнэ</p>
+                  <ChevronDown className="size-6 text-iconPrimary"/>
+                </div>
+                <div  className="flex gap-2 py-2 px-3">
+                  <Calendar className="size-6 text-iconPrimary"/>
+                  <p>Сараар</p>
+                  <ChevronDown className="size-6 text-iconPrimary"/>
+                </div>
+              </div>
+              <div className="w-[419px] flex gap-2 items-center p-2 bg-white rounded-lg">
+                <Search className="size-6 text-iconPrimary"/>
+                <Input placeholder="Бүтээгдэхүүний нэр, SKU, UPC" />
+              </div>
+            </div>
             <Table className="flex-1">
               <TableHeader className="border-b-2 border-[#ECEDF0 ]">
                 <TableRow>
