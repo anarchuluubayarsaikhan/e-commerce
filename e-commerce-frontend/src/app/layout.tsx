adee4d45a846d36e9fe827a9dc22af838552cb5d
odbaatar tulgaa <metatulgaa@gmail.com>
2024-09-23 12:58:53 +0800

    category

--- e-commerce-frontend/src/app/layout.tsx
+++ e-commerce-frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Header } from "@/components/header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,10 +29,8 @@ export default function RootLayout({
       <body
         className={` ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="max-w-[1440px] m-auto">
-          {children}
-        </div>
-
+        <Header />
+        {children}
       </body>
     </html>
   );
