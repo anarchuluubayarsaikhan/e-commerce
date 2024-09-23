b4416cb1fefab144330b93ea67bce49bfdc44e9f
24LP2121 24LP2121 <24LP2121@administrators-iMac-3.local>
2024-09-23 17:22:15 +0800

    update

--- e-commerce-frontend/src/app/page.tsx
+++ e-commerce-frontend/src/app/page.tsx
@@ -1,8 +1,6 @@
-
-
-import { Product } from "@/components/productDash";
-
-
+import { Header } from "@/components/header";
+import { Listitem } from "@/components/listitem";
+import { TableHeader } from "@/components/ui/table";
 
 export default function Home() {
   return (
