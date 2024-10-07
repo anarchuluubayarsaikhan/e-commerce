c526bb477da9cd4e59c0468c55546fe2112ed8d9
24LP2121 24LP2121 <24LP2121@administrators-iMac-3.local>
2024-10-07 16:36:17 +0800

    user part

--- e-commerce-frontend/src/app/page.tsx
+++ e-commerce-frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Header } from "@/components/header";
-import { Listitem } from "@/components/sidebar";
+import { Userpart } from "@/components/userPart";
 
 export default function Home() {
   return (
     <div className="max-w-[1440px] mx-auto">
       <Header />
-      <Listitem />
+      <Userpart />
     </div>
   );
 }
